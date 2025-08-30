9db600947f987c1820136e258fb00a89ed9302ad
Fahad-108 <fahiiraza8@gmail.com>
2025-08-30 13:27:46 +0500

    update

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dukaan-digital-topaz.vercel.app/",
+  baseURL: "https://dukaan-digital-topaz.vercel.app/api",
   headers: { "Content-Type": "application/json" },
 });
 
