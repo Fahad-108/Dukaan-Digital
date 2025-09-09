b9ac39f8c511ac2ebaf660aae9a3af02ff62bb7e
Fahad-108 <fahiiraza8@gmail.com>
2025-09-09 19:09:27 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://fahad-108.github.io', 'http://localhost:5173'],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
