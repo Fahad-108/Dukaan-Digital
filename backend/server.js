bcc77039744381860e18d1e5be5755e642c2b57f
Fahad-108 <fahiiraza8@gmail.com>
2025-09-13 15:56:57 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true          
-}));
+}));   
     
 app.get('/', (req, res) => {             
     res.send('Welcome to Server');
